09320e98c42d28b2b3c9bba7241ea8ae6b3c945c
KJ <36408549+ForbiddenKJ@users.noreply.github.com>
2020-08-20 23:32:50 +0100

    Add files via upload

---
+++ example.py
@@ -0,0 +1,24 @@
+from ws import *
+
+# Setting up json
+dataHandler = jsonLoader('dictionary_compact.json') # Json file
+
+dataHandler.load_data() # Loads json file into a list
+
+fullData = dataHandler.writeList
+
+rLetters = input("Anagram > ")
+
+bruteHandler = wordBrute(rLetters)
+
+bruteHandler.brute() # Brute forcing all possible cominations
+
+bruteList = bruteHandler.writeList
+
+correctWord = bruteHandler.solve(fullData) # Comparing compinations to the json file keys
+
+correctWord = sorted(correctWord, key=len) # Sorts the list into length of order
+
+for i in correctWord:
+    if len(i) > 2:
+        print(i+': '+str(len(i)))
